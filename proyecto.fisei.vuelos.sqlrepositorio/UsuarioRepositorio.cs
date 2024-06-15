46d588d51465091098e09a185e4808347bff19b7
Alexa <alexarieltonato@hotmail.com>
2024-06-15 16:25:43 -0500

    Se agrega la funcionalidad del usuario

--- proyecto.fisei.vuelos.sqlrepositorio/UsuarioRepositorio.cs
+++ proyecto.fisei.vuelos.sqlrepositorio/UsuarioRepositorio.cs
@@ -8,6 +8,7 @@ using proyecto.fisei.vuelos.dominio;
 using System.Data;
 using System.Data.SqlClient;
 using Dapper;
+using System.Security.Cryptography;
 
 namespace proyecto.fisei.vuelos.sqlrepositorio
 {
@@ -15,26 +16,57 @@ namespace proyecto.fisei.vuelos.sqlrepositorio
     {
         IEnumerable<Usuario> IUsuarioRepositorio.ListarUsuarios()
         {
-            using (IDbConnection conexion = new SqlConnection(ConexionRepository.ObtenerCadenaConexion()))
+            try
             {
-                conexion.Open();
-                var usuario = conexion.Query<Usuario>("dbo.listarUsuarios", commandType: CommandType.StoredProcedure);
-                return usuario;
+                using (IDbConnection conexion = new SqlConnection(ConexionRepository.ObtenerCadenaConexion()))
+                {
+                    conexion.Open();
+                    var usuario = conexion.Query<Usuario>("dbo.listarUsuarios", commandType: CommandType.StoredProcedure);
+                    return usuario;
+                }
+            }catch (Exception ex)
+            {
+                throw ex;
             }
         }
 
         Usuario IUsuarioRepositorio.RegistrarUsuario(Usuario usuario)
         {
-            using (IDbConnection conexion = new SqlConnection(ConexionRepository.ObtenerCadenaConexion()))
+            try
             {
-                conexion.Open();
-                var parametros = new DynamicParameters();
-                parametros.Add("Nombre", usuario.Nombre);
-                parametros.Add("Apellido", usuario.Apellido);
-                parametros.Add("CorreoElectronico", usuario.CorreoElectronico);
-                parametros.Add("ClaveHash", usuario.ClaveHash);
-                var usuarioRegistrado = conexion.Query<Usuario>("dbo.insertarUsuario", parametros, commandType: CommandType.StoredProcedure).FirstOrDefault();
-                return usuario;
+                using (IDbConnection conexion = new SqlConnection(ConexionRepository.ObtenerCadenaConexion()))
+                {
+                    conexion.Open();
+                    var parametros = new DynamicParameters();
+                    parametros.Add("Nombre", usuario.Nombre);
+                    parametros.Add("Apellido", usuario.Apellido);
+                    parametros.Add("CorreoElectronico", usuario.CorreoElectronico);
+                    parametros.Add("ClaveHash",HashearClave(usuario.ClaveHash));
+                    var usuarioRegistrado = conexion.Query<Usuario>("dbo.insertarUsuario", parametros, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                    usuario.ClaveHash = HashearClave(usuario.ClaveHash);
+                    return usuario;
+                }
+            }
+            catch 
+            {
+                return new Usuario { Nombre = "El correo esta duplicado"};
+            }
+        }
+
+        private string HashearClave(string clave)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convertir la clave a un arreglo de bytes
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(clave));
+
+                // Convertir los bytes a un string hexadecimal
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
             }
         }
     }
